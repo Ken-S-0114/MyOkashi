1f3881e51ed75a4cbe3decd9dfd79ae919892a22
Ken-S-0114 <satoken@satoukennoMacBook-Pro.local>
2017-04-24 00:42:22 +0900

    Initial Commit

---
+++ MyOkashi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyOkashi
+//
+//  Created by 佐藤賢 on 2017/04/24.
+//  Copyright © 2017年 佐藤賢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
